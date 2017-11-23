6732170b995a4ebe647567f75f40579e0cb0f973
Dan Harris <Dan.Harris@hfoh.com.au>
2017-11-23 22:25:36 +1030

    feat(base-styling): added base styling for app components + initial spacing & layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,20 +5,41 @@ import styled from "styled-components";
 import profileImage from "../assets/profile-image.png";
 
 // styled directives
+const Wrapper = styled.div`
+  max-width: 345px;
+`;
+const TitleWrapper = styled.div`
+  display: inline-block;
+  padding-left: ${props => props.theme.padding * 0.25}rem;
+`;
 const ProfileImage = styled.img`
   height: 100%;
   max-height: 70px;
   border-radius: 50%;
-  border: 2px solid;
+  border: 4px solid ${props => props.theme.color.primary};
+`;
+const Title = styled.h1`
+  font-weight: 500;
+  font-size: 3rem;
+  color: ${props => props.theme.color.accent2};
+  float: right;
+  margin: 0;
+`;
+const TitlePrimaryColor = styled.span`
+  color: ${props => props.theme.color.primary};
 `;
 
 class Header extends Component {
   render() {
     return (
-      <div>
+      <Wrapper>
         <ProfileImage src={profileImage} alt="logo" />
-        <h1>danharris.io</h1>
-      </div>
+        <TitleWrapper>
+          <Title>
+            <TitlePrimaryColor>danharris</TitlePrimaryColor>.io
+          </Title>
+        </TitleWrapper>
+      </Wrapper>
     );
   }
 }
--- src/containers/IconTray.js
+++ src/containers/IconTray.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+// app imports
+
+// styled directives
+const Wrapper = styled.div`
+  margin: ${props => props.theme.padding}rem 0;
+`;
 
 class IconTray extends Component {
   render() {
-    return <p>IconTray</p>;
+    return <Wrapper>IconTray</Wrapper>;
   }
 }
 
