53a48852b18a066ff2a90b268a91f6b301cdd0ab
Dan Harris <Dan.Harris@hfoh.com.au>
2017-11-29 00:11:03 +1030

    feat(icontray-links): added links to icontray icons

--- src/containers/IconTray.js
+++ src/containers/IconTray.js
@@ -10,6 +10,12 @@ import {
 // app imports
 import { media } from "../containers/LandingApp.theme";
 
+// links for icons
+const TWITTER_LINK = "https://twitter.com/danharris_io";
+const GITHUB_LINK = "https://github.com/dan-harris";
+const MEDIUM_LINK = "https://medium.com/@danharris_io";
+const LINKEDIN_LINK = "https://www.linkedin.com/in/dan-harris";
+
 // styled directives
 const Wrapper = styled.div`
   margin: ${props => props.theme.padding}rem 0;
@@ -24,16 +30,31 @@ const Wrapper = styled.div`
     padding: 0 ${props => props.theme.padding}rem;
   `};
 `;
+const IconLink = styled.a`
+  &:hover {
+    fill-opacity: 0.9;
+  }
+`;
 
 class IconTray extends Component {
   render() {
     return (
       <Wrapper>
-        <Twitter size="large" />
-        <LinkedinOption size="large" />
-        <Medium size="large" />
-        <Github size="large" />
-        <MailOption size="large" />
+        <IconLink href={TWITTER_LINK} target="_blank">
+          <Twitter size="large" />
+        </IconLink>
+        <IconLink href={LINKEDIN_LINK} target="_blank">
+          <LinkedinOption size="large" />
+        </IconLink>
+        <IconLink href={MEDIUM_LINK} target="_blank">
+          <Medium size="large" />
+        </IconLink>
+        <IconLink href={GITHUB_LINK} target="_blank">
+          <Github size="large" />
+        </IconLink>
+        <IconLink href={LINKEDIN_LINK} target="_blank">
+          <MailOption size="large" />
+        </IconLink>
       </Wrapper>
     );
   }
