d63b5da20981629e55d664facd046fbb55ed8ff1
Dan Harris <Dan.Harris@hfoh.com.au>
2018-01-14 15:45:43 +1030

    feat(google-analytics): added google analytics to site

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ReactGA from "react-ga";
 // app imports
 import LandingApp from "./containers/LandingApp";
 import registerServiceWorker from "./registerServiceWorker";
@@ -10,3 +11,7 @@ ReactDOM.render(<LandingApp />, document.getElementById("root"));
 
 // register service worker
 registerServiceWorker();
+
+// initialise google analytics
+ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
+ReactGA.pageview(window.location.pathname);
