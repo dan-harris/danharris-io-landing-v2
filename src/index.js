877d0ac5ce68b3253ef26427c63932fcb0eba84a
Dan Harris <Dan.Harris@hfoh.com.au>
2018-07-08 22:17:38 +0930

    feat(static-render): moved site to a static snapshot using react-snap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { hydrate, render } from "react-dom";
 import ReactGA from "react-ga";
 // app imports
 import LandingApp from "./containers/LandingApp";
@@ -7,10 +7,17 @@ import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
 
 // render app
-ReactDOM.render(<LandingApp />, document.getElementById("root"));
-
-// register service worker
-registerServiceWorker();
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  // give a nice 'fade in' effect to page content
+  rootElement.style.opacity = "1";
+  // hydrate the react app (mostly useless at the moment TODO: dynamic api calls for fun)
+  hydrate(<LandingApp />, rootElement);
+  // register service worker
+  registerServiceWorker();
+} else {
+  render(<LandingApp />, rootElement);
+}
 
 // initialise google analytics
 ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
